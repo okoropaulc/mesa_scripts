f4b200bfb19340542f2529cb56c879b11d1ed6a1
okoropaulc@yahoo.com <okoropaulc@yahoo.com>
2019-08-20 11:38:14 -0500

    updates

--- 2nd_grid_compare.R
+++ 2nd_grid_compare.R
@@ -157,7 +157,7 @@ write.table(knn_comp, file="Z:/data/mesa_models/python_ml_models/merged_chunk_re
 
 #full elnet AFA chrom
 elnet <- read.table(file = "Z:/data/mesa_models/split_mesa/results/all_chr_AFA_model_summaries.txt", header = TRUE)
-
+rf_com <- read.table(file="Z:/data/mesa_models/python_ml_models/merged_chunk_results/best_grid_rf_all_chrom.txt", header = T)
 #Random forest
 
 #sort by gene_id
@@ -189,8 +189,9 @@ names(el_rf_filt) <- c("gene_id", "elnet_cv_R2", "rf_cv_R2")
 #hist(el_rf_filt, breaks = "FD", xlim = c(-1, 1), freq = T, xlab = "CV R2", main = "Baseline (Elnet) Performance on MESA AFA all chr")
 #hist(rf_comp$CV_R2, breaks = "FD", xlim = c(-1, 1), freq = T, xlab = "CV R2", main = "Random Forest Performance on MESA AFA all chr")
 
-ggplot(el_rf_filt, aes(x=elnet_cv_R2, y=rf_cv_R2)) + ggtitle("Elastic Net vs Random Forest (outliers (< -1) filtered off)") + 
-  ylab("RF R2") + xlab("Elastic Net R2") +
+ggplot(el_rf_filt, aes(x=elnet_cv_R2, y=rf_cv_R2)) + 
+  ggtitle("Elastic Net vs Random Forest (outliers (< -1) filtered off) Grid Search") + 
+  ylab("RF R2 Grid Optimized") + xlab("Elastic Net R2") +
   geom_point(shape=1) + geom_abline(slope=1,intercept=0,col='blue') + xlim(c(-1, 1)) + ylim(c(-1,1))
 
 #check the correlation of the two: elnet_filt and rf_filt
@@ -199,7 +200,8 @@ library("ggpubr")
 ggscatter(el_rf_filt, x = "elnet_cv_R2", y = "rf_cv_R2", 
           add = "reg.line", add.params = list(color="red"), conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Elastic Net R2", ylab = "RF R2", title = "Elastic Net vs Random Forest (outliers (< -1) filtered off)",
+          xlab = "Elastic Net R2", ylab = "RF R2 Grid Optimized", 
+          title = "Elastic Net vs Random Forest (outliers (< -1) filtered off) Grid Search",
           xlim = c(-1, 1), ylim = c(-1, 1)) + geom_abline(intercept = 0, slope = 1, color="blue")
 
 #SVR
@@ -223,11 +225,9 @@ library(dplyr)
 el_svr_filt <- inner_join(elnet_filt, svr_filt, by = c("gene_id" = "Gene_ID"))
 names(el_svr_filt) <- c("gene_id", "elnet_cv_R2", "svr_cv_R2")
 
-#hist(el_rf_filt, breaks = "FD", xlim = c(-1, 1), freq = T, xlab = "CV R2", main = "Baseline (Elnet) Performance on MESA AFA all chr")
-#hist(rf_comp$CV_R2, breaks = "FD", xlim = c(-1, 1), freq = T, xlab = "CV R2", main = "Random Forest Performance on MESA AFA all chr")
-
-ggplot(el_svr_filt, aes(x=elnet_cv_R2, y=svr_cv_R2)) + ggtitle("Elastic Net vs SVM (outliers (< -1) filtered off)") + 
-  ylab("SVM R2") + xlab("Elastic Net R2") +
+ggplot(el_svr_filt, aes(x=elnet_cv_R2, y=svr_cv_R2)) + 
+  ggtitle("Elastic Net vs SVM (outliers (< -1) filtered off) Grid Search") + 
+  ylab("SVM R2 Grid Optimized") + xlab("Elastic Net R2") +
   geom_point(shape=1) + geom_abline(slope=1,intercept=0,col='blue') + xlim(c(-1, 1)) + ylim(c(-1,1))
 
 #check the correlation of the two: elnet_filt and rf_filt
@@ -236,5 +236,122 @@ library("ggpubr")
 ggscatter(el_svr_filt, x = "elnet_cv_R2", y = "svr_cv_R2", 
           add = "reg.line", add.params = list(color="red"), conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Elastic Net R2", ylab = "SVM R2", title = "Elastic Net vs SVM (outliers (< -1) filtered off)",
+          xlab = "Elastic Net R2", ylab = "SVM R2 Grid Optimized", 
+          title = "Elastic Net vs SVM (outliers (< -1) filtered off) Grid Search",
+          xlim = c(-1, 1), ylim = c(-1, 1)) + geom_abline(intercept = 0, slope = 1, color="blue")
+
+
+#KNN
+#sort the dataframe by gene id
+knn_comp <- knn_comp[order(knn_comp$Gene_ID),]
+elnet <- elnet[order(elnet$gene_id),]
+
+#filter the R2 to be within -1 and 1
+
+knn_filt <- subset(knn_comp, CV_R2 >= -1)
+
+
+knn_filt <- knn_filt[,c(1,3)]
+elnet_filt <- elnet_filt[,c(1,10)]
+
+knn_filt$Gene_ID <- as.character(knn_filt$Gene_ID)
+elnet_filt$gene_id <- as.character(elnet_filt$gene_id)
+
+library(dplyr)
+
+el_knn_filt <- inner_join(elnet_filt, knn_filt, by = c("gene_id" = "Gene_ID"))
+names(el_knn_filt) <- c("gene_id", "elnet_cv_R2", "knn_cv_R2")
+
+ggplot(el_knn_filt, aes(x=elnet_cv_R2, y=knn_cv_R2)) + 
+  ggtitle("Elastic Net vs KNN (outliers (< -1) filtered off) Grid Search") + 
+  ylab("KNN R2 Grid Optimized") + xlab("Elastic Net R2") +
+  geom_point(shape=1) + geom_abline(slope=1,intercept=0,col='blue') + xlim(c(-1, 1)) + ylim(c(-1,1))
+
+#check the correlation of the two: elnet_filt and rf_filt
+
+library("ggpubr")
+ggscatter(el_knn_filt, x = "elnet_cv_R2", y = "knn_cv_R2", 
+          add = "reg.line", add.params = list(color="red"), conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Elastic Net R2", ylab = "KNN R2 Grid Optimized", 
+          title = "Elastic Net vs SVM (outliers (< -1) filtered off) Grid Search",
+          xlim = c(-1, 1), ylim = c(-1, 1)) + geom_abline(intercept = 0, slope = 1, color="blue")
+
+
+#Chapter 3
+#compare the grid optimized model with the un-optimized ones
+#read in saved files
+#RF
+rf <- read.table(file = "Z:/data/mesa_models/python_ml_models/results/rf_cv_full_AFA_chr.txt", header = TRUE)
+
+rf_comp <- rf_comp[order(rf_comp$Gene_ID),]
+rf <- rf[order(rf$Gene_ID),]
+
+#filter the R2 to be within -1 and 1
+
+knn_filt <- subset(knn_comp, CV_R2 >= -1)
+
+
+orf <- rf_comp[,c(1,3)]
+rf <- rf[,c(1,2)]
+
+orf$Gene_ID <- as.character(orf$Gene_ID)
+rf$Gene_ID <- as.character(rf$Gene_ID)
+
+library(dplyr)
+
+orf_rf <- inner_join(orf, rf, by = c("Gene_ID" = "Gene_ID"))
+names(orf_rf) <- c("gene_id", "orf_cv_R2", "rf_cv_R2")
+
+ggplot(orf_rf, aes(x=orf_cv_R2, y=rf_cv_R2)) + 
+  ggtitle("optimized RF vs non optimized RF") + 
+  ylab("Non Optimized RF") + xlab("Optimized RF R2") +
+  geom_point(shape=1) + geom_abline(slope=1,intercept=0,col='blue') + xlim(c(-1, 1)) + ylim(c(-1,1))
+
+#check the correlation of the two: elnet_filt and rf_filt
+
+library("ggpubr")
+ggscatter(orf_rf, x = "orf_cv_R2", y = "rf_cv_R2", 
+          add = "reg.line", add.params = list(color="red"), conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Optimized RF R2", ylab = "Non Optimized RF", 
+          title = "optimized RF vs non optimized RF",
+          xlim = c(-1, 1), ylim = c(-1, 1)) + geom_abline(intercept = 0, slope = 1, color="blue")
+
+
+#RF
+knn <- read.table(file = "Z:/data/mesa_models/python_ml_models/results/knn_cv_full_AFA_chr.txt", header = TRUE)
+
+knn_comp <- knn_comp[order(knn_comp$Gene_ID),]
+knn <- knn[order(knn$Gene_ID),]
+
+#filter the R2 to be within -1 and 1
+
+#knn_filt <- subset(knn_comp, CV_R2 >= -1)
+
+
+oknn <- knn_comp[,c(1,3)]
+knn <- knn[,c(1,2)]
+
+oknn$Gene_ID <- as.character(oknn$Gene_ID)
+knn$Gene_ID <- as.character(knn$Gene_ID)
+
+library(dplyr)
+
+oknn_knn <- inner_join(oknn, knn, by = c("Gene_ID" = "Gene_ID"))
+names(oknn_knn) <- c("gene_id", "oknn_cv_R2", "knn_cv_R2")
+
+ggplot(oknn_knn, aes(x=oknn_cv_R2, y=knn_cv_R2)) + 
+  ggtitle("optimized KNN vs non optimized KNN") + 
+  ylab("Non Optimized KNN") + xlab("Optimized KNN R2") +
+  geom_point(shape=1) + geom_abline(slope=1,intercept=0,col='blue') + xlim(c(-1, 1)) + ylim(c(-1,1))
+
+#check the correlation of the two: elnet_filt and rf_filt
+
+library("ggpubr")
+ggscatter(oknn_knn, x = "oknn_cv_R2", y = "knn_cv_R2", 
+          add = "reg.line", add.params = list(color="red"), conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Optimized KNN R2", ylab = "Non Optimized KNN", 
+          title = "optimized KNN vs non optimized KNN",
           xlim = c(-1, 1), ylim = c(-1, 1)) + geom_abline(intercept = 0, slope = 1, color="blue")
