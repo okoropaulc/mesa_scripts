5cab2d683f8ffdd17cbb522dc71143d9e2f479d1
okoropaulc@yahoo.com <okoropaulc@yahoo.com>
2019-12-18 17:29:36 -0600

    updated mesa pheno wrangle script

--- mesa_pheno_ml_script.R
+++ mesa_pheno_ml_script.R
@@ -1,29 +1,37 @@
-lipid_pheno <- read.csv(file = "Z:/data/mesa_models/mesa_pheno/Exam1Main.csv", header = T)
+#lipid_pheno <- read.csv(file = "Z:/data/mesa_models/mesa_pheno/Exam1Main.csv", header = T)
 
-thrombomodulin <- read.table(file = "Z:/data/mesa_models/mesa_pheno/thrombotic/MESA_thrombotic_phenotypes.txt", header=T)
+thrombomodulin <- read.table(file = "/home/pokoro/data/mesa_models/mesa_pheno/thrombotic/MESA_thrombotic_phenotypes.txt", header=T)
 
 #check CAU dosages for thrombomodulin
+print("check CAU dosages for thrombomodulin")
 
-caudos1 <- read.table(file = "Z:/data/mesa_models/mesa_pheno/thrombotic/chr1txt.gz", header=T, nrows = 3)
+caudos1 <- read.table(file = "/home/pokoro/data/mesa_models/mesa_pheno/thrombotic/chr1txt.gz", header=T, nrows = 3)
+header3 <- colnames(caudos1)[1:6] #store the names of the first six column
+
 caudos1[,c(1:6)] <- NULL
+
+
 
 #strsplit returns list, and to access it, use [[]] to access a row and [] to access a column
 
 #change the colnames of cau dosages to be numbers only
+print("change the colnames of cau dosages to be numbers only")
 
 for (i in 1:length(caudos1)){
   colnames(caudos1)[i] <- strsplit(colnames(caudos1)[i], "_")[[1]][2]
 }
 
-mcaudos1 <- read.table(file = "Z:/data/mesa_models/cau/CAU_1_snp.txt", header=T, nrow=1)
+mcaudos1 <- read.table(file = "/home/pokoro/data/mesa_models/cau/CAU_1_snp.txt", header=T, nrow=1)
 mcaudos1$id <- NULL
 
 #change the colnames to numbers only
+print("change the colnames to numbers only")
 for (i in 1:length(mcaudos1)){
   colnames(mcaudos1)[i] <- strsplit(colnames(mcaudos1)[i], "X")[[1]][2]
 }
 
 #read in the columns
+print("read in the columns")
 caucol <- colnames(caudos1)
 caucol <- sort(caucol)
 
@@ -41,17 +49,21 @@ mcaucol <- sort(mcaucol)
 #write.table(mcaucol, file = "c:/Users/okoro/OneDrive/Desktop/mcaulcols.txt", row.names = FALSE)
 #fcau <- subset(caudos1, select = -c(cauinter))
 
+print("keep sample ID's that are not in the dosages used to build model")
 fcau <- caudos1[, !(names(caudos1) %in% mcaucol)] #keep sample ID's that are not in the dosages used to build model
 
 #drop NA in phenotype
-
+print("drop NA in phenotype")
 library(tidyverse)
 
+print("drop NA")
 thrombomodulin <- drop_na(thrombomodulin) #drop NA
 
 #Now keep only sample ID in dosages that are in phenotype
+print("Now keep only sample ID in dosages that are in phenotype")
 fcau <- fcau[, (names(fcau) %in% thrombomodulin$sidno)]
 
+print("rename the row with the sample ID")
 rownames(thrombomodulin) <- thrombomodulin$sidno #rename the row with the sample ID
 
 #Drop rows where sample ID is not the phenotype dosages
@@ -62,9 +74,26 @@ remthromb <- thrombomodulin[(rownames(thrombomodulin) %in% names(fcau)),]
 
 remid <- names(fcau) #write the remaining sample IDs
 
+header <- c(header3, remid) #combine the first 6 column with the remaining sample ID
 
 #Now read in the genotype files and select only the sample IDs with phenotype
 
 library(data.table)
 
-caupheno <- fread(file = "Z:/data/mesa_models/mesa_pheno/thrombotic/chr1txt.gz", header=T, select = remid, nrows = 3)
+#use loop to go through all the 22 chromosomes
+"%&%" <- function(a,b) paste(a,b, sep = "")
+
+for (i in 1:22){
+  print(i)
+  
+  caupheno <- fread(file = "/home/pokoro/data/mesa_models/mesa_pheno/thrombotic/chr" %&% i %&% "txt.gz", header=T, showProgress=T)
+  for (i in 1:length(caupheno)){
+    colnames(caupheno)[i] <- strsplit(colnames(caupheno)[i], "_")[[1]][2]
+  }# just to convert the columns to only string of numbers
+  
+  caupheno <- caupheno[, (names(caupheno) %in% header)] #keep only columns that in the header
+  #Now write the dosage file back
+  
+  write.table(caupheno, file = "/home/pokoro/data/mesa_models/mesa_pheno/thrombotic/cau_dosage_chr" %&% i %&% ".txt", quote=F, row.names=F, sep ="\t")
+  
+}
