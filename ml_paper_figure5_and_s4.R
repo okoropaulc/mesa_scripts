cf1a1c03a92c1ea525c0a4630eba1a320e0a9d3d
okoropaulc@yahoo.com <okoropaulc@yahoo.com>
2020-04-15 22:40:30 -0500

    updates

--- ml_paper_figure5_and_s4.R
+++ ml_paper_figure5_and_s4.R
@@ -180,10 +180,10 @@ knn_qq <- ggplot(knn_qq_input,aes(x=Expected,y=Observed)) + geom_point(shape=1) 
 #plot all 4 plots in one page
 library(gridExtra)
 fig <- grid.arrange(en_qq,rf_qq,svr_qq,knn_qq,nrow=2)
-#print(fig)
-#tiff("/Users/okoro/OneDrive/Desktop/mesa_scripts/ml_paper_figs/FigS5.tiff", width = 16, height = 16, units = 'cm', res = 300, compression = 'lzw')
-#fig
-#dev.off()
+print(fig)
+tiff("/Users/okoro/OneDrive/Desktop/mesa_scripts/ml_paper_figs/FigS5.tiff", width = 16, height = 16, units = 'cm', res = 300, compression = 'lzw')
+fig
+dev.off()
 
 en_qq_input <- mutate(en_qq_input, model="Elastic Net")
 rf_qq_input <- mutate(rf_qq_input, model="Random Forest")
@@ -203,4 +203,78 @@ fig <- ggplot(data,aes(x=Expected,y=Observed)) + geom_point(shape=1) + theme_cla
 fig
 tiff("/Users/okoro/OneDrive/Desktop/mesa_scripts/ml_paper_figs/FigS5.tiff", width = 16, height = 16, units = 'cm', res = 300, compression = 'lzw')
 fig
+dev.off()
+
+
+#####Make the manhattan plot
+library(readr)
+library(ggrepel)
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
+
+en_df.tmp <- enassocman %>% group_by(CHR) %>% summarise(chr_len=max(BP)) %>% mutate(tot=cumsum(chr_len)-chr_len) %>% select(-chr_len) %>% 
+  left_join(enassocman, ., by=c("CHR"="CHR")) %>% arrange(CHR, BP) %>% mutate( BPcum=BP+tot) %>%
+  mutate( is_highlight=ifelse(SNP %in% NA, "yes", "no")) %>% mutate( is_annotate=ifelse(P < 1e-6, "yes", "no"))
+
+en_axisdf <- en_df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+
+rf_df.tmp <- rfassocman %>% group_by(CHR) %>% summarise(chr_len=max(BP)) %>% mutate(tot=cumsum(chr_len)-chr_len) %>% select(-chr_len) %>% 
+  left_join(rfassocman, ., by=c("CHR"="CHR")) %>% arrange(CHR, BP) %>% mutate( BPcum=BP+tot) %>%
+  mutate( is_highlight=ifelse(SNP %in% NA, "yes", "no")) %>% mutate( is_annotate=ifelse(P < 1e-6, "yes", "no"))
+
+rf_axisdf <- rf_df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+
+svr_df.tmp <- svrassocman %>% group_by(CHR) %>% summarise(chr_len=max(BP)) %>% mutate(tot=cumsum(chr_len)-chr_len) %>% select(-chr_len) %>% 
+  left_join(svrassocman, ., by=c("CHR"="CHR")) %>% arrange(CHR, BP) %>% mutate( BPcum=BP+tot) %>%
+  mutate( is_highlight=ifelse(SNP %in% NA, "yes", "no")) %>% mutate( is_annotate=ifelse(P < 1e-6, "yes", "no"))
+
+svr_axisdf <- svr_df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+
+knn_df.tmp <- knnassocman %>% group_by(CHR) %>% summarise(chr_len=max(BP)) %>% mutate(tot=cumsum(chr_len)-chr_len) %>% select(-chr_len) %>% 
+  left_join(knnassocman, ., by=c("CHR"="CHR")) %>% arrange(CHR, BP) %>% mutate( BPcum=BP+tot) %>%
+  mutate( is_highlight=ifelse(SNP %in% NA, "yes", "no")) %>% mutate( is_annotate=ifelse(P < 1e-6, "yes", "no"))
+
+knn_axisdf <- knn_df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+
+en_gg <- ggplot(en_df.tmp, aes(x=BPcum, y=-log10(P))) + geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
+  scale_color_manual(values = rep(c("red","blue"), 22 )) + scale_x_continuous( label = en_axisdf$CHR, breaks= en_axisdf$center ) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0,14), breaks = seq(0,14,2)) + ggtitle(paste0("Elastic Net")) + labs(x = "Chromosome") +
+  geom_hline(yintercept = -log10(5e-8), colour="red") +
+  #geom_hline(yintercept = -log10(1e-6), linetype="dashed") +
+  geom_label_repel(data=en_df.tmp[en_df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) + 
+  theme_classic(base_size = 22) + theme(legend.position = "none", axis.text.x = element_blank()) + ylab(expression(~~-log[10](italic(p))))
+
+
+rf_gg <- ggplot(rf_df.tmp, aes(x=BPcum, y=-log10(P))) + geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
+  scale_color_manual(values = rep(c("red","blue"), 22 )) + scale_x_continuous( label = rf_axisdf$CHR, breaks= rf_axisdf$center ) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0,14), breaks = seq(0,14,2)) + ggtitle(paste0("Random Forest")) + labs(x = "Chromosome") +
+  geom_hline(yintercept = -log10(5e-8), colour="red") +
+  #geom_hline(yintercept = -log10(1e-6), linetype="dashed") +
+  geom_label_repel(data=rf_df.tmp[rf_df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) + 
+  theme_classic(base_size = 22) + theme(legend.position = "none", axis.text.x = element_blank()) + ylab(expression(~~-log[10](italic(p))))
+
+svr_gg <- ggplot(svr_df.tmp, aes(x=BPcum, y=-log10(P))) + geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
+  scale_color_manual(values = rep(c("red","blue"), 22 )) + scale_x_continuous( label = svr_axisdf$CHR, breaks= svr_axisdf$center ) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0,14), breaks = seq(0,14,2)) + ggtitle(paste0("Support Vector")) + labs(x = "Chromosome") +
+  geom_hline(yintercept = -log10(5e-8), colour="red") +
+  #geom_hline(yintercept = -log10(1e-6), linetype="dashed") +
+  geom_label_repel(data=svr_df.tmp[svr_df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) + 
+  theme_classic(base_size = 22) + theme(legend.position = "none", axis.text.x = element_blank()) + ylab(expression(~~-log[10](italic(p))))
+
+knn_gg <- ggplot(knn_df.tmp, aes(x=BPcum, y=-log10(P))) + geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
+  scale_color_manual(values = rep(c("red","blue"), 22 )) + scale_x_continuous( label = knn_axisdf$CHR, breaks= knn_axisdf$center ) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0,14), breaks = seq(0,14,2)) + ggtitle(paste0("K Nearest Neighbor")) + labs(x = "Chromosome") +
+  geom_hline(yintercept = -log10(5e-8), colour="red") +
+  #geom_hline(yintercept = -log10(1e-6), linetype="dashed") +
+  #geom_label_repel(data=knn_df.tmp[knn_df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) + 
+  theme_classic(base_size = 22) + theme(legend.position = "none", axis.text.x = element_blank()) + ylab(expression(~~-log[10](italic(p))))
+
+
+#plot all 4 plots in one page
+library(gridExtra)
+#fig <- grid.arrange(en_gg,rf_gg,svr_gg,knn_gg,nrow=2)
+#print(fig)
+tiff("/Users/okoro/OneDrive/Desktop/mesa_scripts/ml_paper_figs/Fig5.tiff", width = 24, height = 16, units = 'cm', res = 300, compression = 'lzw')
+grid.arrange(en_gg,rf_gg,svr_gg,knn_gg,nrow=2)
 dev.off()
